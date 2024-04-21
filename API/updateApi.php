e3a3b5a8eea4d0e408cc21eb33603ab543801e70
Janak67 <chandegrajanak6742@gmail.com>
2024-04-21 13:42:04 +0530

    new

--- API/updateApi.php
+++ API/updateApi.php
@@ -2,27 +2,36 @@
 
     include("../connection.php");
 
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $prize = $_POST['prize'];
-    $qty = $_POST['qty'];
-    $rate = $_POST['rate'];
-    $category = $_POST['category'];
-
-    if($id!=null&&$name!=null&&$prize!=null&&$qty!=null&&$rate!=null&&$category!=null)
+    if(isset($_POST['id'], $_POST['name'], $_POST['prize'], $_POST['qty'], $_POST['category'], $_POST['rate']))
     {
-        $query =  "UPDATE product SET(`name`='$name',`prize`='$prize',`qty`='$qty',`rate`='$rate',`category`='$category') WHERE `id`='$id'";
-        $result = mysqli_query($conn,$query);
-    
-        if($result)
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $prize = $_POST['prize'];
+        $qty = $_POST['qty'];
+        $category = $_POST['category'];
+        $rate = $_POST['rate'];
+
+        $query = "UPDATE product SET name='$name', prize='$prize', qty='$qty', rate='$rate', category='$category' WHERE id='$id'";
+        $res = mysqli_query($conn, $query);
+
+        if($res)
         {
-          echo "Succesfully Data Update";
-          header("index.php");
+            $msg = array('status'=>'Success');
+            echo json_encode($msg);
+            http_response_code(200);
         }
         else
         {
-          echo "Failed to Data Update";
+            $msg = array('status'=>'Failed');
+            echo json_encode($msg);
+            http_response_code(400);
         }
     }
-    
+    else
+    {
+        $msg = array('status'=>'Key not Exists');
+        echo json_encode($msg);
+        http_response_code(404);
+    }
+
 ?>
